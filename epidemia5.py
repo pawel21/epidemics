f00d8c0fd6a3d8c7b122181bd3ef1e54d21386cd
Pawel <pawel@pawel-pc.home>
2017-01-30 15:32:06 +0100

    add diffrent initial condition

--- epidemia5.py
+++ epidemia5.py
@@ -175,3 +175,61 @@ def symulacja_bez_szczepien_i_szczepienia_zalezne_od_czasu():
 
     plt.subplots_adjust(left=0.08, right=0.93, top=0.93, bottom=0.1, hspace=0.29, wspace=0.320)
     plt.show()
+
+def symulacja_bez_szczepienia_rozne_warunki_poczatkowe():
+    # Warunki poczatkowe 1
+    P_01 = 1000
+    N_01 = 50
+    W_01 = 0
+    M_01 = 0
+
+    a = 0.0005
+    b = 1 / 10.0
+    z = 0.05
+    alpha = 0.002
+    beta = 0.0001
+
+    czas_w_dniach = np.linspace(0, 80, 1000)
+    y_01 = P_01, N_01, W_01, M_01  # wektor warunków poczatkowych
+    rozwiazanie_1 = odeint(uklad_rownan_bez_szczepienia, y_01, czas_w_dniach, args=(a, b, z, alpha, beta))
+    P_1, N_1, W_1, M_1 = rozwiazanie_1.T
+
+    # wykres dla przypadku bez szczepienia
+    fig = plt.figure(facecolor='w')
+    ax = fig.add_subplot(121, axis_bgcolor='#dddddd', axisbelow=True)
+    ax.plot(czas_w_dniach, P_1, 'b', alpha=0.5, lw=2, label="Podatni")
+    ax.plot(czas_w_dniach, N_1, 'r', alpha=0.5, lw=2, label="Zarażeni")
+    ax.plot(czas_w_dniach, W_1, 'g', alpha=0.5, lw=2, label="Uzdrowieni")
+    ax.plot(czas_w_dniach, M_1, 'k', alpha=0.5, lw=2, label="Zgony")
+    ax.set_ylim([0, 1350])
+    ax.set_xlabel("Czas [dni]")
+    ax.set_ylabel("Liczba osób")
+    ax.set_title("Bez szczepienia")
+    ax.text(50, 750, 'a={0}\nb={1} \nz={2} \n alpha={3} \n beta={4} \n P_0={5} \n N_0={6} \n W_0={7} \n M_0={8}'.format(
+        a, b, z, alpha, beta, P_01, N_01, W_01, M_01), bbox={'facecolor': 'red', 'alpha': 0.5, 'pad': 10})
+    plt.legend(loc="upper left", prop={'size': 20})
+
+    # Warunki poczatkowe 1
+    P_02 = 1000
+    N_02 = 250
+    W_02 = 0
+    M_02 = 0
+
+    y_02 = P_02, N_02, W_02, M_02  # wektor warunków poczatkowych
+    rozwiazanie_2 = odeint(uklad_rownan_bez_szczepienia, y_02, czas_w_dniach, args=(a, b, z, alpha, beta))
+    P_2, N_2, W_2, M_2 = rozwiazanie_2.T
+
+    ax1 = fig.add_subplot(122, axis_bgcolor='#dddddd', axisbelow=True)
+    ax1.plot(czas_w_dniach, P_2, 'b', alpha=0.5, lw=2, label="Podatni")
+    ax1.plot(czas_w_dniach, N_2, 'r', alpha=0.5, lw=2, label="Zarażeni")
+    ax1.plot(czas_w_dniach, W_2, 'g', alpha=0.5, lw=2, label="Uzdrowieni")
+    ax1.plot(czas_w_dniach, M_2, 'k', alpha=0.5, lw=2, label="Zgony")
+    ax1.set_ylim([0, 1350])
+    ax1.set_xlabel("Czas [dni]")
+    ax1.set_ylabel("Liczba osób")
+    ax1.set_title("Bez szczepienia")
+    ax1.text(50, 900, 'a={0}\nb={1} \nz={2} \n alpha={3} \n beta={4} \n P_0={5} \n N_0={6} \n W_0={7} \n M_0={8}'.format(
+        a, b, z, alpha, beta, P_02, N_02, W_02, M_02), bbox={'facecolor': 'red', 'alpha': 0.5, 'pad': 10})
+    plt.legend(loc="upper left", prop={'size': 20})
+
+    plt.show()
